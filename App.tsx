8c42f3d2a08fe9f137c6205c7bf2a603030cdcaf
Marcelo Andrade <marcelo.andrade.ter@estacio.br>
2021-06-03 19:19:07 -0300

    ajuste estrutura

--- App.tsx
+++ App.tsx
@@ -6,19 +6,24 @@
  * @flow strict-local
  */
 import React from 'react';
-import type {Node} from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
+import type { Node } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/screens/Home';
+const Stack = createStackNavigator();
 
 
 const App: () => Node = () => {
 
   return (
-      <View>
-        <Text>foi</Text>
-      </View>
+    <NavigationContainer>
+      <PaperProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </PaperProvider>
+    </NavigationContainer>
   );
 };
 
--- index.tsx
+++ index.tsx
@@ -1,21 +1,10 @@
 /**
  * @format
  */
+import * as React from 'react';
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
-import { NavigationContainer } from '@react-navigation/native';
-import { Provider as PaperProvider } from 'react-native-paper';
+import App from './App'
 
-export default function Main() {
-    return (
-      <NavigationContainer>
-          <PaperProvider>
-            <App />
-          </PaperProvider>
-      </NavigationContainer>
-    );
-  }
-
-AppRegistry.registerComponent(appName, () => Main );
+AppRegistry.registerComponent(appName, () => App );
